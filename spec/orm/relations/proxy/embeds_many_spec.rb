754eefedd156c0ed5bb3bb0a9e4aad5cdf7e3a23
Andrzej <andrzej@cluemarket.com>
2011-04-27 18:04:30 -0700

    more test cases for adding records to collection

--- spec/orm/relations/proxy/embeds_many_spec.rb
+++ spec/orm/relations/proxy/embeds_many_spec.rb
@@ -8,8 +8,8 @@ describe TestEmbedsManyProxy do
 
   let(:proxy_owner) { Person.new :id => "person-id-1", :name => "Test", :age => 29 }
   let(:proxy_target) { Address.new :id => "address-1",:street => "Main St." }
-  let(:proxy_target_2) { Address :id => "address-2" }
-  let(:proxy_target_3) { Address :id => "address-3" }
+  let(:proxy_target_2) { Address.new :id => "address-2",:street => "Left St." }
+  let(:proxy_target_3) { Address.new :id => "address-3",:street => "Middle St." }
   let(:metadata) { subject.metadata }
 
   subject { proxy_owner.send(:relation_proxy, 'addresses') }
@@ -34,7 +34,6 @@ describe TestEmbedsManyProxy do
 					proxy_owner.addresses << proxy_target
 					proxy_owner.save!
 					subject.send(add_method, proxy_target)
-					#proxy_owner.reload
 					proxy_owner = Person.find ("person-id-1")
 					proxy_owner.addresses.should include(proxy_target)
 				end
@@ -44,6 +43,50 @@ describe TestEmbedsManyProxy do
 					proxy_owner.should be_new_record
 				end
 
+				it "should not do anything adding the same record twice" do
+					2.times { subject.send(add_method, proxy_target) }
+					subject.proxy_target.length.should == 1
+					proxy_owner.addresses.length.should == 1
+				end
+
+				it "should be able to add two records at the same time" do
+					subject.send add_method, [proxy_target, proxy_target_2]
+					subject.proxy_target.should include proxy_target
+					subject.proxy_target.should include proxy_target_2
+				end
+
+				it "should return proxy so calls can be chained" do
+          subject.send(add_method, proxy_target).object_id.should == subject.object_id
+        end
+
+        it "should raise an error if there is a type mismatch" do
+          lambda { subject.send add_method, Person.new(:name => "Foo", :age => 2) }.should raise_error MassiveRecord::ORM::RelationTypeMismatch
+        end
+
+        it "should not save the pushed proxy_target if proxy_owner is not persisted" do
+          proxy_owner.should_receive(:persisted?).and_return false
+          proxy_target.should_not_receive(:save)
+          subject.send(add_method, proxy_target)
+        end
+
+        it "should not save the proxy_owner object if it has not been persisted before" do
+          proxy_owner.should_receive(:persisted?).and_return false
+          proxy_owner.should_not_receive(:save)
+          subject.send(add_method, proxy_target)
+        end
+
+        it "should not save anything if one record is invalid" do
+          proxy_owner.save!
+
+          proxy_target.should_receive(:valid?).and_return(true)
+          proxy_target_2.should_receive(:valid?).and_return(false)
+
+          proxy_target.should_not_receive(:save)
+          proxy_target_2.should_not_receive(:save)
+          proxy_owner.should_not_receive(:save)
+
+          subject.send(add_method, [proxy_target, proxy_target_2]).should be_false
+        end
 			end
 		end
 	end
